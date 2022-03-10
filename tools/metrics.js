b5a35fb54c97a0bdce60e60c52b62378598ee69a
cgoern <goern@fastmail.net>
2022-03-10 16:05:01 +0100

    Add family option; Remove configurable namespace

--- tools/metrics.js
+++ tools/metrics.js
@@ -1,7 +1,8 @@
 const fontkit = require('fontkit')
 const fonts = [
-  fontkit.openSync('tests/fonts/Inter-roman.var.woff2'),
-  fontkit.openSync('tests/fonts/Merriweather-roman.var.woff2'),
+  fontkit.openSync('src/assets/fonts/Inter-normal.var.woff2'),
+  fontkit.openSync('src/assets/fonts/Merriweather-normal.var.woff2'),
+  fontkit.openSync('src/assets/fonts/FiraCode.var.woff2'),
 ]
 
 fonts.map((font) => {
