f2b45394d7afd44ec66b60f0e413f0ca6287e130
cgoern <goern@fastmail.net>
2022-05-13 09:33:22 +0200

    Dynamically set heading levels in rich text component

--- tools/metrics.js
+++ tools/metrics.js
@@ -2,7 +2,7 @@ const fontkit = require('fontkit')
 const fonts = [
   fontkit.openSync('assets/fonts/Inter-normal.var.woff2'),
   fontkit.openSync('assets/fonts/Merriweather-normal.var.woff2'),
-  fontkit.openSync('assets/fonts/FiraCode.var.woff2'),
+  fontkit.openSync('assets/fonts/RobotoMono-normal.var.woff2'),
 ]
 
 fonts.map((font) => {
